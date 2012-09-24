c703bcfc849d4aed7d92547c1a7c2a46ee3291a9
Chris Sun <mrozma@gmail.com>
2012-09-25 00:15:19 +0800

    sing up

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -18,4 +18,8 @@ describe "LayoutLinks" do
 	get '/help'
 	response.should have_selector('title', :content => "Help")
   end
+  it "should have a Help page at '/signup'" do
+	get '/signup'
+	response.should have_selector('title', :content => "Sign Up")
+  end
 end
